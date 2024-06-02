4cd8a8b24b341e0c5cde3c5fbdc76de027a3df80
253507605 <13295129572@163.com>
2024-06-02 23:29:35 +0800

    增加nacos配置中心和dynamictp

--- springdemo/src/main/java/com/springdemo/SpringDemoApp.java
+++ springdemo/src/main/java/com/springdemo/SpringDemoApp.java
@@ -1,5 +1,6 @@
 package com.springdemo;
 
+import org.dromara.dynamictp.core.spring.EnableDynamicTp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.springdemo.controller","com.jason.common.config"})
 @EnableDiscoveryClient
+@EnableDynamicTp
 public class SpringDemoApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoApp.class);
--- springdemo/src/main/java/com/springdemo/controller/HelloController.java
+++ springdemo/src/main/java/com/springdemo/controller/HelloController.java
@@ -1,17 +1,34 @@
 package com.springdemo.controller;
 
+import org.dromara.dynamictp.core.executor.DtpExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Author Jason.Chen
  * @create 2024/5/30 15:29
  */
 @RestController
+@RefreshScope//nacos自动刷新
 public class HelloController {
+
+    @Value("${jason.chen}")
+    private String jason;
+
+    @Resource
+    private DtpExecutor dtpExecutor;
 
     @GetMapping("/hello")
     public String hello(){
+        System.out.println(jason);
+        dtpExecutor.execute(()->{
+            System.out.println("hello world");
+        });
         return "hello world!";
     }
 }
